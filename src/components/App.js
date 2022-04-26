1373294aa149fed9d472d52ddcb219cfea843026
Matt Eilar <meilar@gmail.com>
2022-04-26 15:51:36 -0700

    Refactor header

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import './../App.css';
 import Header from './Header';
 import OilControl from './OilControl';
-import ShopInfo from './ShopInfo'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
@@ -13,7 +12,6 @@ function App() {
           <Header />
         <div className="row">
           <div className="col-4">
-            <ShopInfo />
           </div>
           <div className="col-8">
            <OilControl />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,15 @@ import React from "react";
 function Header() {
   return (
     <React.Fragment>
-      <div className="jumbotron">
-        <h1 className="display-4"> Pierre's Automotive</h1>
-        <p className="text-lead">Oiling you up since 1986.</p>
+      <div className='row main-header'>
+        <div className='col-4 header-text'>
+          <p className="font-weight-bold">Oiling you up since 1986.</p>
+          <p className="text-lead">Pierre's Automotive carries a wide variety of automotive oils.
+          Click on the oil name in the list on the left to see a detailed view.</p>
+        </div>
+        <div className='col-8'>
+         <h1 className="display-4"> Pierre's Automotive</h1>
+        </div>
       </div>
     </React.Fragment>
   )
